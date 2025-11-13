ce8768f3167f1bce5668f436872ed2c1f8c829ab
GBLucas1809 <gblucas180902@gmail.com>
2025-11-13 18:19:18 -0300

    feat(Divergence): Add a shield prototype for Defense Mode (Temperance)

--- scripts/mechanics/divergence/defense_mode.gd
+++ scripts/mechanics/divergence/defense_mode.gd
@@ -3,8 +3,16 @@ extends Node
 static var is_mode_on = false
 
 
-static func defend():
-	# do some defense
-	print("Defended!")
-	
-	is_mode_on = true
+static func get_defense_shield(node_path):
+	pass
+
+static func defense(node_path):
+	var _shield_node = get_defense_shield(node_path)
+
+	if not _shield_node:
+		print("Node not found...")
+		return null
+	 
+	if _shield_node.visible == false: 
+		_shield_node.visible
+
